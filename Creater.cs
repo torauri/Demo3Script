2aac073eb3d256869aaf662bae50459bc32610f6
moritomo <moritomo@square-enix.com>
2017-12-08 17:26:36 +0900

    実験用

--- Creater.cs
+++ Creater.cs
@@ -6,20 +6,49 @@ public class Creater : MonoBehaviour {
 	public GameObject enemyPrefab;
 	public int interval;
 	private int frame;
+	private float Espeed;
+	private float Eposy;
 	// Use this for initialization
 	void Start () {
+		interval = 0;
 		frame = 0;
+		Espeed = 0.3f;
+		Eposy = 0.5f;
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		frame++;
-		if(frame%interval==0){
-			float r=Random.Range(0.5f,1.3f);
+		interval-=1;
+
+		if(Input.GetKey("return") && interval <= 0){
+			Debug.Log("gene");
+			float r=Eposy;
 			GameObject enemy = Instantiate(enemyPrefab,new Vector3(transform.position.x,transform.position.y+r,transform.position.z),transform.rotation);
-            enemy.GetComponent<Demo2enemy>().speed =Random.Range(0.3f,0.5f);
-            //interval = (int)Random.Range(30, 80);
-          //  frame = 0;
+            enemy.GetComponent<Demo2enemy>().speed =Espeed;
+			interval = 30;
 		}
+
+		//速さの調整
+		if(Input.GetKey("q")){
+			Espeed = 0.6f;
+		}
+		if(Input.GetKey("w")){
+			Espeed = 0.45f;
+		}
+		if(Input.GetKey("e")){
+			Espeed = 0.3f;
+		}
+
+		//高さの調整
+		if(Input.GetKey("a")){
+			Eposy = 1.3f;
+		}
+		if(Input.GetKey("s")){
+			Eposy = 0.5f;
+		}
+		if(Input.GetKey("d")){
+			Eposy = 0.9f;
+		}
+
 	}
 }
